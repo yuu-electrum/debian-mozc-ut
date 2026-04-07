c8ee6d38e839f59e60aa2d4b166ffc73efd297ea
Yu Morino / Red Face Programmers <thirteenth.nickel@proton.me>
2026-04-07 14:58:53 +0900

    change: install-fcitx5.sh now handles other container runner like Podman

--- install-fcitx5.sh
+++ install-fcitx5.sh
@@ -1,6 +1,8 @@
-docker run -itd --name debian-mozc-ut debian-mozc-ut:latest
-docker cp debian-mozc-ut:/build-packages/. ./build-packages
+OCI_CONTAINER="${RUNNER:docker}"
+
+$OCI_CONTAINER run -itd --name debian-mozc-ut debian-mozc-ut:latest
+$OCI_CONTAINER cp debian-mozc-ut:/build-packages/. ./build-packages
 sudo dpkg -i build-packages/mozc-data*.deb build-packages/mozc-server*.deb build-packages/mozc-utils-gui*.deb build-packages/fcitx5-mozc*.deb build-packages/fcitx-mozc-data_*.deb
-docker stop debian-mozc-ut
-docker rm debian-mozc-ut
+$OCI_CONTAINER stop debian-mozc-ut
+$OCI_CONTAINER rm debian-mozc-ut
 rm -rf build-packages
